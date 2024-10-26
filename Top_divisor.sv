60663f42ddaa5ecc5988495792c08e8e00d40b58
Adrian Cruz <japeacr1@teleco.upv.es>
2024-10-26 14:19:06 +0200

    Add files via upload

--- Top_divisor.sv
+++ Top_divisor.sv
@@ -1,21 +1,21 @@
 `timescale 1ns/1ps
 
-// M贸dulo superior
+// M骴ulo superior
 module Top_divisor();
     parameter tamanyo = 32;
     logic CLK;
     logic RSTa;
 
-    // Instanciaci贸n de la interfaz
+    // Instanciaci髇 de la interfaz
     Interface_if #(tamanyo) test_if(.reloj(CLK), .reset(RSTa));
 
-    // Instanciaci贸n del dise帽o (DUT)
+    // Instanciaci髇 del dise駉 (DUT)
     Divisor_Algoritmico Duv (.bus(test_if));
 
-    // Instanciaci贸n del programa de est铆mulos
+    // Instanciaci髇 del programa de est韒ulos
     estimulos #(tamanyo) estim1(.testar(test_if),.monitorizar(test_if));
 
-    // Generaci贸n del reloj (CLK)
+    // Generaci髇 del reloj (CLK)
     always begin
         CLK = 1'b0; #5;
         CLK = 1'b1; #5;
@@ -33,12 +33,10 @@ module Top_divisor();
         $dumpfile("divisor.vcd");
         $dumpvars(1, Top_divisor.Duv.divisor_duv.Coc);
         $dumpvars(1, Top_divisor.Duv.divisor_duv.Res);
-//    	$dumpvars(1, Top_divisor.estim1.casos.sb.target_coc); // Observando el valor esperado de Cociente
-//    	$dumpvars(1, Top_divisor.estim1.casos.sb.target_res); // Observando el valor esperado de Residuo
     end
 endmodule
 
-// Definici贸n de la interfaz/////////////////////////////////////////////////////////////////////////////////////////
+// Definici髇 de la interfaz/////////////////////////////////////////////////////////////////////////////////////////
 interface Interface_if #(parameter tamanyo = 32) (input bit reloj, input bit reset);
     logic Start;
     logic Done;
@@ -57,7 +55,7 @@ interface Interface_if #(parameter tamanyo = 32) (input bit reloj, input bit res
         input #1ns Done;
     endclocking: md;
 
-    // Clocking block para generaci贸n de est铆mulos 
+    // Clocking block para generaci髇 de est韒ulos 
     clocking sd @(posedge reloj);
         input  #2ns Coc;
         input  #2ns Res;
@@ -81,22 +79,22 @@ interface Interface_if #(parameter tamanyo = 32) (input bit reloj, input bit res
     );
 endinterface
 
-// Paquete de verificaci贸n//////////////////////////////////////////////////////////////////////////////////////////
+// Paquete de verificaci髇//////////////////////////////////////////////////////////////////////////////////////////
 package utilidades_verificacion;
     parameter tamanyo = 32;
 
     class RCSG;
-        rand logic signed [tamanyo-1-20:0] num_rand;
-        rand logic signed [tamanyo-1-20:0] den_rand;
+        rand logic signed [tamanyo-1:0] num_rand;
+        rand logic signed [tamanyo-1:0] den_rand;
         constraint den_valido { den_rand != 0; }
     endclass
 
     class Scoreboard;
-	logic [tamanyo-1:0] cola_target_coc [$];
-	logic [tamanyo-1:0] cola_target_res [$];
-	logic [tamanyo-1:0] pretarget_coc, pretarget_res;
-        logic [tamanyo-1:0] target_coc, target_res;
-    	logic [tamanyo-1:0] observado_Coc, observado_Res;
+ 	logic signed[tamanyo-1:0] cola_target_coc [$];
+	logic signed[tamanyo-1:0] cola_target_res [$];
+	logic signed[tamanyo-1:0] pretarget_coc, pretarget_res;
+        logic signed[tamanyo-1:0] target_coc, target_res;
+    	logic signed[tamanyo-1:0] observado_Coc, observado_Res;
 
         virtual Interface_if.monitor mports;
 
@@ -109,31 +107,43 @@ package utilidades_verificacion;
 
 
 
-        task monitor_input;
-            while (1) begin
-                @(mports.md);
-                if (mports.md.Start) begin
-                    pretarget_coc = mports.md.Num / mports.md.Den;
-                    pretarget_res = mports.md.Num % mports.md.Den;
-		    cola_target_coc.push_front(pretarget_coc);
- 		    cola_target_res.push_front(pretarget_res);
-                end
-            end
-        endtask
-
-        task monitor_output;
-            while (1) begin
-                @(mports.md);
-                if (mports.md.Done) begin
-		    target_coc= cola_target_coc.pop_back();
-		    target_res= cola_target_res.pop_back();
-       		    observado_Coc = mports.md.Coc;
-		    observado_Res = mports.md.Res;
-                    assert (observado_Coc == target_coc) else $error("Cociente incorrecto: Esperado %d, Observado %d", target_coc, observado_Coc);
-                    assert (observado_Res == target_res) else $error("Residuo incorrecto: Esperado %d, Observado %d", target_res, observado_Res);
-                end
-            end
-        endtask
+	task monitor_input;
+	    
+	    logic start_control = 0; // Variable para evitar duplicados
+	    while (1) begin
+	        @(mports.md);
+	        if (mports.md.Start) 
+		  if (!start_control) begin // Solo guardar si start_control es 0
+	            pretarget_coc = mports.md.Num / mports.md.Den;
+	            pretarget_res = mports.md.Num % mports.md.Den;
+	            cola_target_coc.push_front(pretarget_coc);
+	            cola_target_res.push_front(pretarget_res);
+//	            $display("Guardamos esto en la cola - Cociente: %d, Residuo: %d", pretarget_coc, pretarget_res);
+		    start_control = 1; // Cambia el estado para evitar duplicados
+	            end 
+		else begin
+                        start_control = 0; // Reiniciar el flag cuando Start se desactiva
+                     end  
+		end   
+	endtask
+	
+	task monitor_output;
+	    while (1) begin
+	        @(mports.md);
+	        if (mports.md.Done) begin
+	                target_coc = cola_target_coc.pop_back();
+	                target_res = cola_target_res.pop_back();
+               	 	observado_Coc = mports.md.Coc;
+               	 	observado_Res = mports.md.Res;
+//	                $display("Valores de la cola - Cociente: %d, Residuo: %d", target_coc, target_res);
+//	                $display("Num: %d, Den: %d, Esperado Coc: %d, Observado Coc: %d", mports.md.Num, mports.md.Den, target_coc, observado_Coc);
+//	                $display("Num: %d, Den: %d, Esperado Res: %d, Observado Res: %d", mports.md.Num, mports.md.Den, target_res, observado_Res);
+	                assert (observado_Coc == target_coc) else $error("Cociente incorrecto: Esperado %d, Observado %d", target_coc, observado_Coc);
+	                assert (observado_Res == target_res) else $error("Residuo incorrecto: Esperado %d, Observado %d", target_res, observado_Res);
+	        end
+	    end
+	endtask
+	
     endclass
 
 
@@ -145,19 +155,19 @@ package utilidades_verificacion;
         covergroup valores @(monitorizar_ports.md);
 	    // Coverpoint para Num
 	    cp1:coverpoint monitorizar_ports.md.Num { 
-//	        bins entre_min_y_0[] = {[-2**(tamanyo-1):-1]};        // Captura todos los n煤meros negativos
+//	        bins entre_min_y_0[] = {[-2**(tamanyo-1):-1]};        // Captura todos los n鷐eros negativos
 	        bins zero[] = {0};                 	     // Captura el cero
-	        bins entre_1_y_100[] = {[1:100]};      	     // Captura n煤meros del 1 al 100
-//	        bins entre_100_y_max[] = {[101:2**(tamanyo-1)]};  // Captura n煤meros mayores que 100
+	        bins entre_1_y_100[] = {[1:100]};      	     // Captura n鷐eros del 1 al 100
+//	        bins entre_100_y_max[] = {[101:2**(tamanyo-1)]};  // Captura n鷐eros mayores que 100
 //		ignore_bins ignorados[] = {[2**(tamanyo):$]};
 	    }
 	
 	    // Coverpoint para Den
 	    cp2:coverpoint monitorizar_ports.md.Den {
-//	        bins entre_min_y_0[] = {[-2**(tamanyo-1):-1]};         // Captura todos los n煤meros negativos
+//	        bins entre_min_y_0[] = {[-2**(tamanyo-1):-1]};         // Captura todos los n鷐eros negativos
 	        illegal_bins zero[] = {0};                            // Captura el cero
-	        bins entre_1_y_100[] = {[1:100]};                // Captura n煤meros del 1 al 100
-//	        bins entre_100_y_max[] = {[101:2**(tamanyo-1)]};   // Captura n煤meros mayores que 100
+	        bins entre_1_y_100[] = {[1:100]};                // Captura n鷐eros del 1 al 100
+//	        bins entre_100_y_max[] = {[101:2**(tamanyo-1)]};   // Captura n鷐eros mayores que 100
 //		ignore_bins ignorados[] = {[2**(tamanyo):$]};
 	    }
 	endgroup
@@ -173,7 +183,7 @@ package utilidades_verificacion;
 	    testar_ports = ports;
             monitorizar_ports = mports;
 
-	    //instanciaci贸n objetos
+	    //instanciaci髇 objetos
 	    busInst = new;               //construimos la clase de valores random
             sb = new(monitorizar_ports); //construimos el scoreboard      
    	    valores = new(); // Instancia del covergroup
@@ -189,11 +199,11 @@ package utilidades_verificacion;
         endtask
 
         task prueba_random;
-   	 int max_iteraciones = 100; // Define un l铆mite
+   	 int max_iteraciones = 100; // Define un l韒ite
    	 int iteraciones = 0;
             while (iteraciones < max_iteraciones) begin
                 assert (busInst.randomize()) else $fatal("Randomization failed");
-		//$display("Num_rand: %0d, Den_rand: %0d", busInst.num_rand, busInst.den_rand);
+//		$display("Num_rand: %0d, Den_rand: %0d", busInst.num_rand, busInst.den_rand);
                 testar_ports.sd.Num <= busInst.num_rand;
                 testar_ports.sd.Den <= busInst.den_rand;
 
@@ -215,16 +225,15 @@ package utilidades_verificacion;
 
 endpackage
 
-// Programa para la instanciaci贸n de m贸dulos//////////////////////////////////////////////////////////////////////////////
+// Programa para la instanciaci髇 de m骴ulos//////////////////////////////////////////////////////////////////////////////
 program estimulos #(parameter tamanyo = 32) (Interface_if.test testar, Interface_if.monitor monitorizar);
     utilidades_verificacion::environment casos = new(testar, monitorizar);
-    
+   
     initial begin
         $display("Iniciando prueba aleatoria...");
 	casos.muestrear;
         casos.prueba_random;
 	$display("Acabando prueba aleatoria...");
-
         $stop;
     end
 endprogram
